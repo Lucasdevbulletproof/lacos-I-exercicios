4ceb6603fc70d397612a1ed5670e268f209fe1c0
Lucasdevbulletproof <[bassolucas1997@gmail.com}>
2022-12-05 15:24:56 -0300

    Resolução dos exercicio de laços 1

---
+++ exercicio2/script.js
@@ -0,0 +1,9 @@
+let tabuada = +prompt("Digite um número para fazer a tabuada!")
+console.log(`Tabuada do ${tabuada}:`.toUpperCase())
+
+for(let i=0 ; i <= 10; i++ ){
+    console.log(`${i} x ${tabuada}=`, tabuada*i)
+
+}
+//RESOLUÇÂO: Criar um prompt que receba um número para ser multiplicado de 0 até 10, e imprima as muultiplicações no console.
+
---
+++ exercicio3/script.js
@@ -0,0 +1,4 @@
+const maiorPaises = ["Russia", "Cadana", "China", "EUA", "Brasil"]
+for (let i = 0; i<maiorPaises.length; i++){
+    console.log(`${i+1} - ${maiorPaises[i]}`)
+}
